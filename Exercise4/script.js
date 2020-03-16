0e29191b0179d1e1eccd29b6c38925b285fd04f0
Amy Leuszler <amyleuszler@ku.edu>
2020-03-16 09:45:34 -0500

    Took out unncessary code

--- Exercise4/script.js
+++ Exercise4/script.js
@@ -8,10 +8,6 @@ var redbodycolor = document.getElementById("redinput").value;
 var greenbodycolor = document.getElementById("greeninput").value;
 var bluebodycolor = document.getElementById("blueinput").value;
 
-console.log(redbordercolor);
-console.log(greenbordercolor);
-console.log(bluebordercolor);
-
 
   document.body.style.backgroundColor= 'rgb(' + redbodycolor + ',' + greenbodycolor + ',' + bluebodycolor + ')';
 
@@ -19,7 +15,4 @@ console.log(bluebordercolor);
 
   document.getElementById("intro").style.borderColor= 'rgb(' + redbordercolor + ',' + greenbordercolor + ',' + bluebordercolor + ')';
 
-  console.log(redbordercolor);
-  console.log(greenbordercolor);
-  console.log(bluebordercolor);
 }
